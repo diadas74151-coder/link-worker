c51e192530e44c4a0e6c00bb7cbcb41b9856054d
diadas74151-coder <diadas74151@gmail.com>
2025-12-29 13:43:15 +0530

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+
 const inputLink = process.argv[2];
 
-// TODO: Replace this with real conversion logic later
 const convertedLink = `https://wishlink.in/demo-converted?src=${encodeURIComponent(inputLink)}`;
+
+// Write output to file for GitHub Actions
+fs.writeFileSync("output.txt", convertedLink);
 
 console.log("CONVERTED_LINK:", convertedLink);
